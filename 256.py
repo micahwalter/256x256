00689a18e3c01a28cd078be205288590cee49963
Micah Walter <micah@micahwalter.com>
2013-04-11 16:41:46 -0400

    setting up random

--- 256.py
+++ 256.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def twofiftysix():
     return 'Hello World!'
+
+@app.route('/random')
+def random():
+	execfile('bin/random256.py')
+	return 'hello random'
---
+++ README.md
@@ -0,0 +1,5 @@
+256x256
+===
+
+This is the httpony part of 256x256.com
+
